d25cc7d418ca8949701354b591724c4c03532632
Madhav Saini <99039829+FireNdIce3@users.noreply.github.com>
2023-07-08 17:04:32 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,13 @@
-import React from "react";
-import "./Header.css";
+import React from 'react'
+import "./Header.css"
 
 function Header() {
   return (
-    <>
-      <div className="header">
-        <div className="brand">
-          <div className="logo"></div>
-          <h1>FESEM</h1>
-        </div>
-       
-      </div>
-      <hr />
-    </>
-  );
+    <div className='header'>
+    <div className='logo'></div>
+    <h1>FESEM</h1>
+    </div>
+  )
 }
 
-export default Header;
+export default Header
